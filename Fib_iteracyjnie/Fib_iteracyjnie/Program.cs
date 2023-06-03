f6a30ead61d8ae802a414cf4bee2c9bf43075d08
ZygmuntCzechura <czechura.zygmunt2@gmail.com>
2023-06-03 20:13:01 +0200

    Added files

---
+++ Fib_iteracyjnie/Fib_iteracyjnie/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fib_iteracyjnie
+{
+    class Program
+    {
+        public static int fib(int n)
+        {
+            int suma = 0;
+            int a = 1, b = 1;
+            if (n < 3) return 1;
+            else
+            for (int i=3;i<=n;i++)
+            {
+                suma = b + a;
+                a = b;
+                b = suma;
+            }
+            return suma;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj który element ciągu: ");
+            int n = int.Parse(Console.ReadLine());
+            int fibnumber =fib(n);
+            Console.WriteLine($"{n} wyraz ciągu to: {fibnumber}");
+        }
+    }
+}
---
+++ fib_rekurencja/fib_rekurencja/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace fib_rekurencja
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj ile elementów ciągu: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Wyraz ciągu ma wartość: {0}",fib(n));
+           
+        }
+
+        public static int fib(int n)
+        {
+            if (n < 3) return 1;
+            else return fib(n - 2) + fib(n - 1);
+        }
+    }
+}
