d7b4529c39081512403727c0b3ea0ff352d87186
ZygmuntCzechura <czechura.zygmunt2@gmail.com>
2023-06-21 21:49:48 +0200

    Added new files

---
+++ Miernik_Energi/Miernik_Energi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Miernik_Energi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj początkowy stan licznika: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj aktualny stan licznika: ");
+            double y = double.Parse(Console.ReadLine());
+            Miernik Stan1 = new Miernik(x,y);
+            Console.WriteLine("Wynik: " + Stan1.Energia());
+            Console.ReadKey();
+        }
+    }
+
+    public class Miernik
+    {
+        private double Start_value { get; set; }
+        private double Now_value { get; set; }
+
+        public Miernik(double Svalue,double Nvalue)
+        {
+            Start_value = Svalue;
+            Now_value = Nvalue;
+        }
+
+        public double Energia()
+        {
+            
+            double n = Start_value - Now_value;
+
+            return n;
+        }
+    }
+}
---
+++ Punkt/Punkt/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Punkt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj wartości punktu: ");
+            Punkt punkt1 = new Punkt(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            punkt1.Wyswietl();
+        }
+    }
+
+    public class Punkt
+    {
+        private int x { get; set;}
+        private int y { get; set;}
+
+
+        public Punkt(int Wspolzedna_x,int Wspolzedna_y)
+        {
+            x = Wspolzedna_x;
+            y = Wspolzedna_y;
+        }
+        public void Wektor()
+        {
+
+            int x_Wektor, y_Wektor;
+            Console.WriteLine("Podaj wartość x Wektora: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wartość y Wektora: ");
+            int z = int.Parse(Console.ReadLine());
+            x_Wektor = x + n;
+            y_Wektor = y + z;
+            Console.WriteLine($"Wynik po przesunięciu: ({x_Wektor},{y_Wektor})");
+        }
+        public void Wyswietl()
+        { 
+            Console.WriteLine($"Punkt: ({x},{y})");
+            Wektor();
+        }
+    }
+}
